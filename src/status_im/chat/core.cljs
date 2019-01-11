1fd56b178d086e58526a86febcfe7a1ad6f05b2d
Igor Mandrigin <i@mandrigin.ru>
2019-01-11 10:06:03 +0100

    fix small stuff

--- src/status_im/chat/core.cljs
+++ src/status_im/chat/core.cljs
@@ -6,22 +6,21 @@
 ;; Seen messages
 (fx/defn receive-seen
   [{:keys [db js-obj]} chat-id sender {:keys [message-ids]}]
-  (merge {}
-         (when-let [seen-messages-ids (-> (get-in db [:chats chat-id :messages])
-                                          (select-keys message-ids)
-                                          keys)]
-           (let [statuses (map (fn [message-id]
-                                 {:chat-id          chat-id
-                                  :message-id       message-id
-                                  :public-key       sender
-                                  :status           :seen})
-                               seen-messages-ids)]
-             {:db            (reduce (fn [acc {:keys [message-id] :as status}]
-                                       (assoc-in acc [:chats chat-id :message-statuses
-                                                      message-id sender]
-                                                 status))
-                                     db
-                                     statuses)
-              :data-store/tx [(user-statuses-store/save-statuses-tx
-                               statuses
-                               #(re-frame/dispatch [:message/message-persisted js-obj]))]}))))
+  (when-let [seen-messages-ids (-> (get-in db [:chats chat-id :messages])
+                                   (select-keys message-ids)
+                                   keys)]
+    (let [statuses (map (fn [message-id]
+                          {:chat-id          chat-id
+                           :message-id       message-id
+                           :public-key       sender
+                           :status           :seen})
+                        seen-messages-ids)]
+      {:db            (reduce (fn [acc {:keys [message-id] :as status}]
+                                (assoc-in acc [:chats chat-id :message-statuses
+                                               message-id sender]
+                                          status))
+                              db
+                              statuses)
+       :data-store/tx [(user-statuses-store/save-statuses-tx
+                        statuses
+                        #(re-frame/dispatch [:message/message-persisted js-obj]))]})))
